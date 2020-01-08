6df6ee59581ce435ba9244477acdc8b1ac44a615
viodart <viodart@gmail.com>
2020-01-08 09:04:45 +0100

    Revert "Camera"
    This reverts commit 358986c94e421e7db11feb460c56d95a2e20fe45.

--- Assets/Scripts/Permission/PhoneCamera.cs
+++ Assets/Scripts/Permission/PhoneCamera.cs
@@ -14,9 +14,6 @@ public class PhoneCamera : MonoBehaviour
     public RawImage m_Background;
     public AspectRatioFitter fit;
     Texture newImage;
-    Image img;
-    public GameObject placeHolder, txtPlaceholder;
-
 
 
     void Start()
