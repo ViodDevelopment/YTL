26a247719c240a12626a4612a935432cfa47888a
Sergio Rodriguez <pekeno1811@gmail.com>
2020-03-12 09:54:32 +0100

    Burbujas to fast

--- Assets/Scripts/BurbujasMinigame/Spawn.cs
+++ Assets/Scripts/BurbujasMinigame/Spawn.cs
@@ -6,17 +6,30 @@ public class Spawn : MonoBehaviour
 {
 
     private GameObject m_Spawn;
-    private float m_Speed = 0.5f;
+    private float m_Speed;
+    public Transform m_Vuelta;
+    bool m_Giro =false;
 
     // Start is called before the first frame update
     void Start()
     {
+        m_Speed = (m_Vuelta.position.x - this.transform.position.x )/ 30 * 2;    
         m_Spawn = GameObject.FindGameObjectWithTag("SpawnGlobo");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Vector3.Distance(this.transform.position, m_Vuelta.position) < 0.15f)
+        {
+            m_Giro = true;
+            this.GetComponent<SpriteRenderer>().flipX=true;
+        }
+        if (!m_Giro)
         transform.position += Vector3.right * Time.deltaTime * m_Speed;
+        else
+        transform.position -= Vector3.right * Time.deltaTime * m_Speed;
+
+       
     }
 }
